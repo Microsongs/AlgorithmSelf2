aed84142e23a18294f51247a9329a743b46a36bb
현석 송 <jhn06046@hotmail.com>
2019-11-01 09:02:48 +0900

    Fibonacci Dynamic Programming

---
+++ AlgorithmSelfStudy/AlgorithmSelfStudy/STL/Baekjoon_2555.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmSelfStudy.STL
+{
+    class Baekjoon_2555
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10/14");
+        }
+    }
+}
