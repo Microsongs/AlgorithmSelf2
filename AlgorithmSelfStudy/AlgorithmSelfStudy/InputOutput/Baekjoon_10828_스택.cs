25a55b65af40c06e6f46ec34eb1bfcff30c3227d
현석 송 <jhn06046@hotmail.com>
2019-12-10 10:20:09 +0900

    백준 10828 스택

---
+++ AlgorithmSelfStudy/AlgorithmSelfStudy/InputOutput/Baekjoon_10828_스택.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmSelfStudy.InputOutput
+{
+    class Baekjoon_10828_스택
+    {
+        // 스택 배열
+        static int[] st;
+        static int top;
+
+        static void Main(string[] args)
+        {
+            st = new int[100001];
+            top = 0;
+            int n = int.Parse(Console.ReadLine());
+            string []order;
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<n; i++)
+            {
+                // 명령어
+                order = Console.ReadLine().Split();
+                switch (order[0])
+                {
+                    case "push":
+                        st[top++] = int.Parse(order[1]);
+                        break;
+                    case "pop":
+                        //Console.Write(top != 0 ? st[--top]+"\n" : "-1\n");
+                        sb.Append(top != 0 ? st[--top] + "\n" : "-1\n");
+                        break;
+                    case "size":
+                        //Console.Write(top + "\n");
+                        sb.Append(top + "\n");
+                        break;
+                    case "empty":
+                        //Console.Write(top != 0 ? "0\n" : "1\n");
+                        sb.Append(top != 0 ? "0\n" : "1\n");
+                        break;
+                    case "top":
+                        //Console.Write(top != 0 ? st[top - 1] + "\n" : "-1\n");
+                        sb.Append(top != 0 ? st[top - 1] + "\n" : "-1\n");
+                        break;
+                }
+            }
+            Console.Write(sb);
+        }
+    }
+}
---
+++ AlgorithmSelfStudy/AlgorithmSelfStudy/InputOutput/Baekjoon_10828_스택v2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmSelfStudy.InputOutput
+{
+    class Baekjoon_10828_스택v2
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            int n = int.Parse(Console.ReadLine());
+            for(int i=0; i<n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] order = input.Split();
+                switch (order[0])
+                {
+                    case "push":
+                        stack.Push(int.Parse(order[1]));
+                        break;
+                    case "top":
+                        Console.Write(stack.Count == 0 ? "-1\n" : stack.Peek() + "\n");
+                        break;
+                    case "size":
+                        Console.Write(stack.Count+"\n");
+                        break;
+                    case "empty":
+                        Console.Write(stack.Count == 0 ? "1\n" : "0\n");
+                        break;
+                    case "pop":
+                        Console.Write(stack.Count == 0 ? "-1\n" : stack.Pop() + "\n");
+                        break;
+                }
+            }
+        }
+    }
+}
